1ec68d1aaae037210a6a773012feb22fc67e91fb
LeonYueh <tommyyueh5@gmail.com>
2022-05-24 16:27:17 +0800

    撈到資料

--- src/api/api.js
+++ src/api/api.js
@@ -5,10 +5,12 @@ import {
 export function GetWeather() {
 
     return requestService({
-        url: 'F-D0047-071',
+        url: 'F-D0047-091',
         method: 'get',
         params: {
-            Authorization: 'CWB-D8B2A879-4B02-4978-99C2-40819A1F00BA'
+            Authorization: 'CWB-D8B2A879-4B02-4978-99C2-40819A1F00BA',
+            format:'JSON',
+            elementName: 'MinT,MaxT'
         }
     })
 }
--- src/components/weekdaylist.vue
+++ src/components/weekdaylist.vue
@@ -1,11 +1,10 @@
 <template>
 
-<H1>123</H1>
-
+    
 </template>
 
 <script setup>
-import { ref } from "vue";
+
 
 </script>
 
